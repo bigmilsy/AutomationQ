c79b868f3d5d5546479f0f5085ab4d448a93f4f4
dmiller <dmiller@192.168.1.3>
2022-05-25 00:00:09 +1000

    Reformat the structure of the Availability test

--- AutomationQ/src/test/java/com/qan/tests/APILatLonAvailabilityTests.java
+++ AutomationQ/src/test/java/com/qan/tests/APILatLonAvailabilityTests.java
@@ -19,7 +19,8 @@ import static org.hamcrest.Matchers.*;
  * @author dmiller
  */
 public class APILatLonAvailabilityTests {
-    private String BaseUrl="http://api.weatherbit.io/v2.0/forecast/hourly?lat=--LAT-DATA--&lon=--LON-DATA--&key=19fca4ec9b474546aecca1d26e92cb2c";
+    //private String BaseUrl="http://api.weatherbit.io/v2.0/forecast/hourly?lat=--LAT-DATA--&lon=--LON-DATA--&key=19fca4ec9b474546aecca1d26e92cb2c";
+    private String BaseUrl="http://api.weatherbit.io/v2.0/forecast/hourly?key=19fca4ec9b474546aecca1d26e92cb2c";
     public TestData.LatLonInputs testData;
     public int expectedStatusCode = 200;
 
@@ -30,10 +31,14 @@ public class APILatLonAvailabilityTests {
     
     @Test
     public void checkAvailability() {
-        //Setup API URL for the test
-        String testUrl = Helpers.buildCurrentURLLatLon(BaseUrl, testData.data.get("checkAvailability").lat, testData.data.get("checkAvailability").lon);
-        //Check to ensure that we see the expected response codel
-        given().when().get(testUrl).then().assertThat().statusCode(expectedStatusCode);
+        given().
+            pathParam("lat", testData.data.get("checkAvailability").lat).
+            pathParam("lon", testData.data.get("checkAvailability").lon).
+        when().
+            get(BaseUrl + "&lat={lat}&lon={lon}").
+        then().
+            assertThat().
+            statusCode(expectedStatusCode);
     }    
     
     @BeforeAll
